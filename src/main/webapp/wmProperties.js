3ec38d1eb382aa487f8e7a5b6f210365ee641edd
root <root@ca445d4f2acb>
2017-06-25 00:46:19 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ylIdorNTvu",
+  "homePage" : "Main",
+  "name" : "Auto_ylIdorNTvu",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
